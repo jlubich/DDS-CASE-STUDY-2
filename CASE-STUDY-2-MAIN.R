d5709ee9840d2c2bd1c6f76cfe6460031feb3614
jlancon <31114052+jlancon@users.noreply.github.com>
2017-11-26 19:13:02 -0600

    Stage Commit

--- CASE-STUDY-2-MAIN.R
+++ CASE-STUDY-2-MAIN.R
@@ -7,6 +7,7 @@
 ## 3 - Identify 3-4 Attrition Rate factors
 ## 4 - Identify 3-4 interesting Job Specific Trends
 ## 5 Conclusions - where focused effort can get you the biggest bang for the buck
+##      Lack of low/med JobPerformance measure
 ## IMPACT
 ##############################################################################
 
@@ -50,6 +51,37 @@ TalentData$EnvironmentSatisfaction.Label <- mapvalues(
   to = c("Low", "Medium", "High", "Very High")
 )
 
+TalentData$JobInvolvement.Label <- mapvalues(
+  TalentData$JobInvolvement, 
+  from = c(1,2,3,4), 
+  to = c("Low", "Medium", "High", "Very High")
+)
+
+TalentData$JobSatisfaction.Label <- mapvalues(
+  TalentData$JobSatisfaction, 
+  from = c(1,2,3,4), 
+  to = c("Low", "Medium", "High", "Very High")
+)
+
+TalentData$PerformanceRating.Label <- mapvalues(
+  TalentData$PerformanceRating, 
+  from = c(1,2,3,4), 
+  to = c("Low", "Good", "Excellent", "Outstanding")
+)
+
+TalentData$RelationshipSatisfaction.Label <- mapvalues(
+  TalentData$RelationshipSatisfaction, 
+  from = c(1,2,3,4), 
+  to = c("Low", "Medium", "High", "Very High")
+)
+
+TalentData$WorkLifeBalance.Label <- mapvalues(
+  TalentData$WorkLifeBalance, 
+  from = c(1,2,3,4), 
+  to = c("Bad", "Good", "Better", "Best")
+)
+
+
 # set up boundaries for intervals/bins
 breaks <- c(0,25,35,45,55,65,1000)
 # specify interval/bin labels
@@ -57,7 +89,26 @@ labels <- c("18-24", "25-34", "35-44", "45-54", "55-64", "65+")
 # bucketing data points into bins
 TalentData$Age.Bin <- cut(TalentData$Age, breaks, include.lowest = T, right=FALSE, labels=labels)
 
+# set up boundaries for intervals/bins
 
+breaks <- c(0,2500,5000,7500,10000,12500,15000,17500,20000,22500,25000,Inf)
+# specify interval/bin labels
+labels <- c("0-2500", "2501-5000", "5001-7500", "7501-10000","10001-12500", "12501-15000","15001-17500","17501-20000","20001-22500","22501-25000","25000+")
+# bucketing data points into bins
+TalentData$MonthlyRate.Bin <- cut(TalentData$MonthlyRate, breaks, include.lowest = T, right=FALSE, labels=labels)
+ac <- table(TalentData$MonthlyRate.Bin)
+barplot(ac)
+
+
+
+
+
+
+
+
+
+ac <- table(TalentData$EducationField)
+barplot(ac)
 
 ?bins.split(TalentData$Age, 8, 0, 2000)
 bins.quantiles(TalentData$Age, 8, 8)
