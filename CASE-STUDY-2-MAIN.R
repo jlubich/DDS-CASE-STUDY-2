2a61aefdccab6e2b7527bbc2409158f1251c2c6e
Will Holt <williamholt@smu.edu>
2017-11-28 21:41:50 -0500

    Update CASE-STUDY-2-MAIN.R

--- CASE-STUDY-2-MAIN.R
+++ CASE-STUDY-2-MAIN.R
@@ -11,6 +11,29 @@
 ## IMPACT
 ##############################################################################
 
+## Executive Summary
+## 2DS analyzed DDSAnalytics' 2016 employee data and determined the top three leading factors that contributed to the organization's attrition rate of 16.12% were: VARIABLE1, VARIABLE2, VARIABLE3. 2DS based this conclusion on the variables that produced the three highest attrition rates relative to the benchmark of 16.12%. Variables that lacked a clear definition or deep understanding were ommitted, but can be included in a refrshed analysis or in any next steps. This is dependant on DDSAnalytcis providing 2DS with sufficient information. The variables ommitted from the analysis included: Daily Rate, Hourly Rate, Monthly Income, Monthly Rate. 
+##Based on these findings, 2DS recommends DDSAnalytics moves forwars with a predictive model that focuses on, but is not limited to the variables mentioned above. As the model is built DDSAnalytics must consider variables that overlap and be sure to only include one. For example, age and the number of companies worked may have a strong overlap and may make it difficult for the organization to properly understand which variable is influencing attrition.
+
+## Background
+## DDSAnalytics contacted 2DS to conduct an exploratory data analysis (EDA) to determine the three leading factors that contributed to attitrion. DDSAnalytics is planning to leverage the insights to approve a project that will leverage data science within the talent management market. 
+
+## Scope
+## 2DS was requested to condcut an EDA on the interaction of 34 variables with attrition. Additional variables were calculated to assist with the analysis. The project did not include statistical analyses, machine learning techniques, or predictive models. 2DS was also requested to report additional trends including those related to job roles.
+
+## Approach
+## 2DS calculated the 2016 attrition rate within the organizaiton as 16.12% and used this as a benchmark to compare other variables. 2DS then calculated attrition rates within each variable to identify if any of the subpopulations had an attrition rate greater than the benchmark. Once all variables were examined, 2DS identified the variables that contained the three highest subpopulations. 
+
+## Key Findings
+## 2DS identified VARIABLE1, VARIABLE2, and VARIABLE3 with the three highest attrition rates within the organizaiton. VARIABLE1, VARIABLE2, and VARIABLE3 had attrition rates of XX.X%, XX.X%, XX.X% and "Yes" attrion response of XX, XX, XX. 
+## FILL IN HERE FOR ADDITIONAL INSIGHTS
+
+
+## Next Steps
+## While the percentages mentioned above represent the three highest attrition rates within DDSAnalytics, it is recommended that a statistical analysis is conducted to determine if the percentages and the number of "Yes" responses are statistically significant. If they are not significant then variable should not be considered. For example, if a variable had an attrition rate or 75% but there were only 3 "Yes" responses DDSAnalytics should not consider this a reliable variable when determining variables that may help build a predictive model.
+## 2DS also recommends that all salary information be listed as yearly income. This will allow for a future analysis that can determine if income influences attrition significantly more than other variables. Based on that new information DDSAnalytics can potentially build a stronger predictive model.
+## 2DS also recommends that DDSAnalytics begins to build and/or strengthen its abaility to integrate machine learning into its Data Science project if it is approved. This will allow for future models to be built through automation, which may reduce cost and increase return on investment.
+
 ## Initialization
 library(plyr)
 library(dplyr)
